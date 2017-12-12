e5eab61853484655f8352e4626919802b3e0424b
Fedor Baart <fedor.baart@deltares.nl>
2017-12-12 10:04:58 +0100

    add story placeholders

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@
       app
     >
       <v-list>
-        <v-list-tile 
+        <v-list-tile
           value="true"
           v-for="(item, i) in items"
           :key="i"
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <router-link :to="`/story/` + i">
+              <v-icon v-html="item.icon"></v-icon>
+            </router-link>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
@@ -24,31 +26,19 @@
     </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
+            <v-icon>fa-globe</v-icon>
             <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
+              &#8220;This is a Sphere.&#8221;
               <footer>
                 <small>
-                  <em>&mdash;John Johnson</em>
+                  <em>&mdash;Fedor</em>
                 </small>
               </footer>
             </blockquote>
@@ -56,21 +46,6 @@
         </v-slide-y-transition>
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
@@ -78,20 +53,25 @@
 </template>
 
 <script>
+  import 'font-awesome/css/font-awesome.css';
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
+        clipped: true,
+        drawer: false,
         fixed: false,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        items: [
+          {
+            icon: 'fa-user-circle',
+            title: 'Story 1'
+          },
+          {
+            icon: 'fa-user-circle',
+            title: 'Story 2'
+          }
+        ],
+        miniVariant: true,
+        title: 'World Water Atlas'
       }
     }
   }
---
+++ src/components/VStory.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-card>
+      <h2>{{ title }}</h2>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'VStory',
+  data () {
+    return {
+      title: 'Story'
+    }
+  }
+}
+
+</script>

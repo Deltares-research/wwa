3e8267d8c8ccec342f2ef947205153197fbffd19
Jurgen Beliën <jurgen@voorhoede.nl>
2018-01-24 16:29:11 +0100

    add theme route with cardlist

---
+++ pages/themes/_theme.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <svg-icon v-if="theme" v-bind:icon="theme" />
+    <card-list v-bind:cards="book.chapters" />
+  </section>
+</template>
+
+<script>
+import CardList from '~/components/card-list/CardList'
+import SvgIcon from '~/components/svg-icon/SvgIcon'
+import book from '~/static/data/books/coastal-safety/index.json'
+
+export default {
+  components: {
+    CardList,
+    SvgIcon
+  },
+  data () {
+    return {
+      book,
+      theme: this.$route.params.theme
+    }
+  }
+}
+</script>

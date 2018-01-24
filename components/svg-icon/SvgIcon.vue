b1d326aab82b9880847c60f021b0d87b750f4531
Jurgen Beliën <jurgen@voorhoede.nl>
2018-01-24 16:14:04 +0100

    move icons to component

---
+++ components/svg-icon/SvgIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg v-bind:class="`svg-icon ${icon} ${iconVariant}`" xmlns:x="http://www.w3.org/1999/xlink">
+    <use v-bind:href="`#${icon}`" />
+  </svg>
+</template>
+
+<script>
+import '~/components/colors/colors.css'
+
+const icon = this.icon
+const variant = this.variant
+
+export default {
+  computed: {
+    iconVariant: () => (this.variant) ? `${icon}--${variant}` : ''
+  },
+  props: [ 'icon', 'variant' ]
+}
+</script>

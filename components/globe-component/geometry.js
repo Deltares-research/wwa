76422342ea4e7a63382b65c19700bfab8bc20342
Fedor Baart <fedor.baart@deltares.nl>
2018-01-25 16:07:07 +0100

    common in JW's code

---
+++ components/globe-component/geometry.js
@@ -0,0 +1,30 @@
+import { scaleLinear } from 'd3-scale'
+import { Vector3 } from 'three'
+
+export function polar2cartesian (radius, latitude, longitude) {
+  const x = radius * Math.sin(latitude) * Math.cos(longitude)
+  const y = radius * Math.sin(latitude) * Math.sin(longitude)
+  const z = radius * Math.cos(latitude)
+
+  return new Vector3(x, y, z)
+}
+
+export function cartesian2polar (x, y, z) {
+  const radius = Math.sqrt((x * x) + (y * y) + (z * z))
+  const latitude = Math.acos(z / radius)
+  const longitude = Math.atan2(y, x)
+
+  return { radius, latitude, longitude }
+}
+
+// degrees to radians (not clipping)
+export const deg2rad = deg => deg * (Math.PI / 180)
+
+// these two functions clip
+export const lat2rad = scaleLinear()
+  .domain([-90, 90])
+  .range([0, Math.PI])
+
+export const lon2rad = scaleLinear()
+  .domain([180, -180])
+  .range([0, 2 * Math.PI])

33d631038f7a7de8fd38bf542c890377c6ae492b
Jurgen Beliën <jurgen@voorhoede.nl>
2018-01-25 10:34:55 +0100

    add page component

---
+++ components/page-component/PageComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <article>
+    <header>
+      <h1>{{page.title}}</h1>
+      <ul class="influences">
+        <li v-for="influence in page.influences" v-bind:key="influence.slug">
+          <nuxt-link v-bind:for="influence.path">{{ influence.title }}</nuxt-link>
+        </li>
+      </ul>
+      <div class="storyteller">
+        {{storyteller}}
+        {{storytellerAvatar}}
+      </div>
+    </header>
+    <section v-if="page.body" class="body">
+      {{body}}
+    </section>
+    <section v-if="page.pictures" class="pictures">
+      <picture v-for="picture in page.pictures" v-bind:key="picture.id">
+        {{picture}}
+      </picture>
+    </section>
+    <section v-if="page.graphs" class="graphs">
+      <figure v-for="graph in page.graphs" v-bind:key="graph.id">
+        {{graph}}
+        <figcaption>{{graph.alt}}</figcaption>
+      </figure>
+    </section>
+    <section v-if="page.video" class="video">
+      {{page.video}}
+    </section>
+    <footer v-if="page.footnotes" class="footnotes">
+      {{page.footnotes}}
+    </footer>
+  </article>
+</template>
+
+<script>
+import '~/components/colors/colors.css'
+
+export default {
+  props: {
+    page: {
+      type: Object
+    }
+    // 'body',
+    // 'endDate',
+    // 'footnotes',
+    // 'graphs',
+    // 'influences',
+    // 'keywords',
+    // 'location',
+    // 'pictures',
+    // 'slug',
+    // 'startDate',
+    // 'storyteller',
+    // 'storytellerAvatar',
+    // 'title',
+    // 'video',
+    // 'zoomLevel'
+  }
+}
+</script>
+
+<style>
+:root {
+  --padding-v: calc(.5rem + .5vw);
+  --padding-h: calc(1rem + 1vw);
+}
+article {
+  background-color: var(--ui-white);
+  padding: var(--padding-v) var(--padding-h);
+  margin: 25vh auto;
+  width: 90vw;
+  min-height: 25vh;
+  min-width: 320px;
+  max-width: 960px;
+}
+</style>
+

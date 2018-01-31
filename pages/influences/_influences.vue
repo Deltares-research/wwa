8424874a7d417b5fae89ae2c93b0df9302f3b227
Jurgen Beliën <jurgen@voorhoede.nl>
2018-01-31 14:21:41 +0100

    switch influences

--- pages/influences/_influences.vue
+++ pages/influences/_influences.vue
@@ -1,14 +1,13 @@
 <template>
   <section>
-    <ul class="inline-list">
+    <ul class="list--inline">
       <li v-for="influence in influences" v-bind:key="influence.slug">
-        <span v-if="activeInfluences.some(active => active === influence.slug)">
+        <span v-if="activeInfluences.some(active => active.slug === influence.slug)">
           {{ influence.title }}
         </span>
         <nuxt-link v-bind:to="influence.path" v-else>
           {{ influence.title }}
         </nuxt-link>
-
       </li>
     </ul>
     <card-list v-bind:cards="results" />
@@ -18,27 +17,25 @@
 <script>
 import CardList from '~/components/card-list/CardList'
 import loadData from '~/lib/load-data'
-import influences from '~/static/data/influences/index.json'
+import allInfluences from '~/static/data/influences/index.json'
 import '~/components/list/list.css'
 
 export default {
   async asyncData (context) {
     const { params } = context
-    const activeInfluences = params.influences.split('+')
-    const { results = [] } = await loadData(context, { influences: activeInfluences })
+    const influencesFromUrl = params.influences.split('+')
+    const { tags, results = [] } = await loadData(context, { influences: influencesFromUrl })
+    // Build active influences objects from url
+    const activeInfluences = tags
+      .filter(tag => influencesFromUrl.some(active => active === tag.slug))
     return {
-      influences,
+      influences: allInfluences,
       activeInfluences,
       results
     }
   },
   components: {
     CardList
-  },
-  data () {
-    return {
-      theme: this.$route.params.theme
-    }
   }
 }
 </script>

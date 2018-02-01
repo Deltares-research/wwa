0b8357dae5858ae0fdecf0d5788ebd903e402522
matthijs schaap <matthijs.schaap@deltares.nl>
2018-02-01 09:47:06 +0100

    added media text and keywords to the page component

--- components/page-component/PageComponent.vue
+++ components/page-component/PageComponent.vue
@@ -1,10 +1,11 @@
 <template>
   <article>
     <header>
+      <div v-bind:class="page.theme+' colorband'"></div>
       <h1>{{page.title}}</h1>
       <ul class="influences">
         <li v-for="influence in page.influences" v-bind:key="influence.slug">
-          <nuxt-link v-bind:to="influence.path">{{ influence.title }}</nuxt-link>
+          <nuxt-link v-bind:for="influence.path">{{ influence.title }}</nuxt-link>
         </li>
       </ul>
       <div class="storyteller" v-if="page.storyteller">
@@ -12,28 +13,34 @@
           v-bind:img="page.storyteller.avatar"
           v-bind:name="page.storyteller.name"
           />
-          {{page.storyteller.name}}
       </div>
     </header>
-    <section v-if="page.body" class="body">
+    <section v-if="page.body" class="body t_margin_2r">
       {{page.body}}
     </section>
-    <section v-if="page.pictures" class="pictures">
-      <picture v-for="picture in page.pictures" v-bind:key="picture.id">
-        {{picture}}
+    <section v-if="page.images" class="images t_margin_2r">
+      <picture v-for="image in page.images" v-bind:key="image.id">
+        <img v-bind:src="image.imgixHost + image.value.path"/>
       </picture>
     </section>
     <section v-if="page.graphs" class="graphs">
       <figure v-for="graph in page.graphs" v-bind:key="graph.id">
-        {{graph}}
+        <img v-bind:src="graph.imgixHost + graph.value.path"/>
         <figcaption>{{graph.alt}}</figcaption>
       </figure>
     </section>
     <section v-if="page.video" class="video">
-      {{page.video}}
+      <img v-bind:src="page.video.thumbnailUrl"/>
     </section>
-    <footer v-if="page.footnotes" class="footnotes">
-      {{page.footnotes}}
+    <footer class="t_margin_2r">
+      <section v-if="page.links" class="links">
+        Links: {{page.links}}
+      </section>
+      <ul v-if="page.keywords" class="keywords">
+        <li v-for="keyword in page.keywords" v-bind:key="keyword.slug">
+          <nuxt-link v-bind:for="keyword.path">{{ keyword.title }}</nuxt-link>
+        </li>
+      </ul>
     </footer>
   </article>
 </template>
@@ -49,7 +56,7 @@ export default {
     }
     // 'body',
     // 'endDate',
-    // 'footnotes',
+    // 'links',
     // 'graphs',
     // 'influences',
     // 'keywords',
@@ -61,6 +68,7 @@ export default {
     // 'storytellerAvatar',
     // 'title',
     // 'video',
+    //  'theme',
     // 'zoomLevel'
   },
   components: {
@@ -79,6 +87,29 @@ article {
   min-height: 25vh;
   min-width: 320px;
   max-width: 960px;
+  position:relative;
 }
+
+.t_margin_2r
+{
+  margin-top: 2rem;
+}
+
+article img
+{
+  display:flex;
+  max-width:100%;
+  margin:0 auto;
+}
+
+.colorband
+{
+  position:absolute;
+  top:0;
+  left:0;
+  right:0;
+  height:32px;
+}
+
 </style>
 

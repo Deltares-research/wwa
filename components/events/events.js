3a9d32f91bb9d13e2f8a58c16384723f4676f258
Fedor Baart <fedor.baart@deltares.nl>
2018-02-08 17:31:31 +0100

    event names

--- components/events/events.js
+++ components/events/events.js
@@ -10,5 +10,7 @@ export const disableGlobeNavigation = 'disable-globe-navigation'
 
 export default {
   activeFeatureChanged,
-  featuresChanged
+  featuresChanged,
+  enableGlobeNavigation,
+  disableGlobeNavigation
 }

e87ac416381e12f4b7eb1b9cb0890863e85b4303
Jurgen Beliën <jurgen@voorhoede.nl>
2018-02-09 19:13:52 +0100

    create page

---
+++ pages/_staticPage.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <article class="static-page">
+    <h1>{{title}}</h1>
+    <section>
+      {{body}}
+    </section>
+  </article>
+</template>
+
+<script>
+import events from '~/components/events/events'
+import loadData from '~/lib/load-data'
+
+export default {
+  async asyncData (context) {
+    const { title, body } = await loadData(context, context.params)
+    return { title, body }
+  },
+  mounted () {
+    this.$events.$emit(events.disableGlobeNavigation)
+  }
+}
+</script>
+<style>
+  @import '../components/colors/colors.css';
+
+  .static-page {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 4rem;
+    background-color: var(--ui--white);
+    box-sizing: border-box;
+  }
+</style>

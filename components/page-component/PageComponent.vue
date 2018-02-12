dc655c8933f73f61783065a8a961d8e9b754f6d7
Celine Kurpershoek <celinekurpershoek@gmail.com>
2018-02-12 15:08:13 +0100

    feat(page): basic html structure for new layout

--- components/page-component/PageComponent.vue
+++ components/page-component/PageComponent.vue
@@ -1,40 +1,46 @@
 <template>
   <article class="page-component" v-bind:class="page.theme.slug">
-    <header>
-       <h1>{{page.title}}</h1>
-      <ul class="influences">
-        <li v-for="influence in page.influences" v-bind:key="influence.slug">
-          <nuxt-link v-bind:to="influence.path">{{ influence.title }}</nuxt-link>
-        </li>
-      </ul>
+    <div class="page-content">
+
+    <section class="page-body">
+      <h1>{{page.title}}</h1>
+      <section v-if="page.body" class="body t_margin_2r">
+        {{page.body}}
+      </section>
+      <section v-if="page.images" class="images t_margin_2r">
+        <picture v-for="image in page.images" v-bind:key="image.id">
+          <img v-bind:src="image.imgixHost + image.value.path"/>
+        </picture>
+      </section>
+      <section v-if="page.graphs" class="graphs">
+        <figure v-for="graph in page.graphs" v-bind:key="graph.id">
+          <img v-bind:src="graph.imgixHost + graph.value.path"/>
+          <figcaption>{{graph.alt}}</figcaption>
+        </figure>
+      </section>
+      <iframe v-if="page.video" class="video"
+      v-bind:src="'https://www.' + page.video.provider + '.com/embed/' + page.video.providerUid"
+      v-bind:width="page.video.width"
+      v-bind:height="page.video.height">
+      </iframe>
+    </section>
+
+    <aside>
       <div class="storyteller" v-if="page.storyteller">
         <avatar-component
           v-bind:img="page.storyteller.avatar"
           v-bind:name="page.storyteller.name"
           />
       </div>
-    </header>
-    <section v-if="page.body" class="body t_margin_2r">
-      {{page.body}}
-    </section>
-    <section v-if="page.images" class="images t_margin_2r">
-      <picture v-for="image in page.images" v-bind:key="image.id">
-        <img v-bind:src="image.imgixHost + image.value.path"/>
-      </picture>
-    </section>
-    <section v-if="page.graphs" class="graphs">
-      <figure v-for="graph in page.graphs" v-bind:key="graph.id">
-        <img v-bind:src="graph.imgixHost + graph.value.path"/>
-        <figcaption>{{graph.alt}}</figcaption>
-      </figure>
-    </section>
-    <iframe v-if="page.video" class="video"
-    v-bind:src="'https://www.' + page.video.provider + '.com/embed/' + page.video.providerUid"
-    v-bind:width="page.video.width"
-    v-bind:height="page.video.height"
-    >
-    </iframe>
-    <footer class="t_margin_2r">
+      <div class="theme">
+        <svg-icon v-if="page.theme && page.theme.slug" class="page-icon" v-bind:icon="page.theme.slug" />
+      </div>
+      <ul class="influences">
+        <li v-for="influence in page.influences" v-bind:key="influence.slug">
+          <nuxt-link v-bind:to="influence.path">{{ influence.title }}</nuxt-link>
+        </li>
+      </ul>
+      <div class="t_margin_2r">
       <section v-if="page.links" class="links">
         Links: {{page.links}}
       </section>
@@ -43,87 +49,88 @@
           <nuxt-link class="tag" v-bind:to="keyword.path">{{ keyword.title }}</nuxt-link>
         </li>
       </ul>
-    </footer>
+    </div>
+    </aside>
+    </div>
   </article>
 </template>
 
 <script>
 import AvatarComponent from '~/components/avatar-component/AvatarComponent'
+import SvgIcon from '~/components/svg-icon/SvgIcon'
 
 export default {
   props: {
-    page: {
-      type: Object
-    }
-    // 'body',
-    // 'endDate',
-    // 'links',
-    // 'graphs',
-    // 'influences',
-    // 'keywords',
-    // 'location',
-    // 'pictures',
-    // 'slug',
-    // 'startDate',
-    // 'storyteller',
-    // 'storytellerAvatar',
-    // 'title',
-    // 'video',
-    //  'theme',
-    // 'zoomLevel'
+    page: { type: Object }
   },
   components: {
-    AvatarComponent
+    AvatarComponent,
+    SvgIcon
   }
 }
 </script>
 
 <style>
 @import '../colors/colors.css';
-
 .page-component {
+
+  position: relative;
+}
+
+.page-content {
   --padding-v: calc(.5rem + .5vw);
   --padding-h: calc(1rem + 1vw);
   background-color: var(--ui--white);
-  color: var(--ui--text);
-  padding: var(--padding-v) var(--padding-h);
-  width: 90vw;
+  color: var(--ui--black);
+  max-width: 944px;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  margin: 0 auto;
   min-height: 25vh;
-  min-width: 320px;
-  max-width: 960px;
-  position:relative;
 }
 
-.page-component::before {
+.page-component:first-of-type:before,
+.page-component:after {
   content: '';
-  position:absolute;
-  top:0;
-  left:0;
-  right:0;
-  height:1rem;
+  height: 20rem;
+  width: 100%;
+  background: linear-gradient(to top, transparent, rgba(0,0,30, .5));
+  position: absolute;
+  bottom: -15rem;
+  z-index: -1;
 }
 
-.page-component.too-little::before {
-  background-color: var(--too-little);
+.page-component:first-of-type:before {
+  background: linear-gradient(to bottom, transparent, rgba(0,0,30, .5));
+  top: -20rem;
 }
 
-.page-component.too-dirty::before {
-  background-color: var(--too-dirty);
+.page-component aside {
+  background: rgb(229,229, 232);
+  padding: var(--padding-v) var(--padding-h);
+  flex-basis: 30%;
 }
 
-.page-component.too-much::before {
-  background-color: var(--too-much);
+.page-component .page-body {
+  padding: var(--padding-v) var(--padding-h);
 }
 
 .page-component .video {
   margin:2rem auto;
 }
 
-.page-component picture img
- {
+.page-component .theme svg {
+  width: 4rem;
+  height: 4rem;
+}
+
+.page-component picture img {
   display:flex;
   max-width:100%;
   margin:0 auto;
 }
+
+
 
 </style>

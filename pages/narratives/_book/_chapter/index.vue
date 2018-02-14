8a4e283ed3611acae212d63ea833c6d48c9b8252
Jurgen Beliën <jurgen@voorhoede.nl>
2018-02-14 11:52:48 +0100

    jump to page inside chapter

--- pages/narratives/_book/_chapter/index.vue
+++ pages/narratives/_book/_chapter/index.vue
@@ -16,41 +16,27 @@ import loadData from '~/lib/load-data'
 
 export default {
   async asyncData (context) {
-    const { pages } = await loadData(context, context.params)
-    const page = pages[0]
-    const book = page.book
-    const chapter = page.chapter
-    const slug = context.params.page || page.slug
-
-    return { book, chapter, pages, slug }
+    const { book, pages, path, slug, title } = await loadData(context, context.params)
+    const chapter = { path, slug, title }
+    return { book, chapter, pages, path, slug, title }
   },
   data () {
-    return {
-      activePage: null
-    }
+    return { activePage: null }
   },
   mounted () {
-    const activePages = this.pages.filter(page => page.slug === this.slug)
-    this.activePage = activePages[0]
+    this.scrollToSlug(this.$route.hash.replace(/^#/, ''))
     this.$events.$emit(events.disableGlobeNavigation)
     this.$events.$emit(events.featuresChanged, this.pages)
+
     if ('IntersectionObserver' in window) {
       this.observeIntersectingChildren()
     }
-    this.updateActiveFeature()
   },
   components: {
     NarrativeHeader,
     PageComponent
   },
   methods: {
-    updateActiveFeature (slug = this.slug) {
-      const activePages = this.pages.filter(page => page.slug === slug)
-      if (activePages) {
-        this.activePage = activePages[0]
-        this.$events.$emit(events.activeFeatureChanged, this.activePage)
-      }
-    },
     observeIntersectingChildren () {
       const intersectionRatio = 0.001
       const observer = new IntersectionObserver(entries => {
@@ -58,7 +44,7 @@ export default {
         // TODO: Pan & Zoom to
       }, {
         // No explicit root, we want the viewport
-        rootMargin: '-5% 0% -5% 0%', // Start from middle of screen
+        rootMargin: '-40% 0% -40% 0%', // Interested in the in the lower part of the screen
         thresholds: [ intersectionRatio ]
       })
       const pageComponentsArray = [].slice.call(this.$el.children)
@@ -68,18 +54,32 @@ export default {
         // No Array.prototype function, so we can break the loop
         for (const entry of entries) {
           if (entry.isIntersecting) {
-            const { base = '/' } = this.$router.options
-            const bookSlug = this.$route.params.book
-            const chapterSlug = this.$route.params.chapter
             const pageSlug = entry.target.id
-            const path = `${base}narratives/${bookSlug}/${chapterSlug}#${pageSlug}`
-            if (path !== window.location.pathname) {
-              history.replaceState({}, 'page', path)
+            if (`#${pageSlug}` !== this.$route.hash) {
+              history.replaceState({}, 'page', `${this.$route.path}#${pageSlug}`)
               this.updateActiveFeature(pageSlug)
             }
             break
           }
         }
+      }
+    },
+    scrollToSlug (slug) {
+      const activePages = this.pages.filter(page => page.slug === slug)
+      const activeElement = document.getElementById(slug)
+      if (activeElement && activePages) {
+        const windowHeight = (window.innerHeight || document.clientHeight)
+        const top = activeElement.getBoundingClientRect().top || windowHeight
+        const y = Math.round(top - (windowHeight / 2)) // match with margin between PageComponents
+        window.scroll(0, y)
+        this.updateActiveFeature(slug)
+      }
+    },
+    updateActiveFeature (slug) {
+      const activePages = this.pages.filter(page => page.slug === slug)
+      if (activePages) {
+        this.activePage = activePages[0]
+        this.$events.$emit(events.activeFeatureChanged, this.activePage)
       }
     }
   }

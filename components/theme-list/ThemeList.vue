58feaa8aa61c6ca4afb0270da354ea1ef208efe6
Jurgen Beliën <jurgen@voorhoede.nl>
2018-02-19 17:15:51 +0100

    try to tweak background on themes

--- components/theme-list/ThemeList.vue
+++ components/theme-list/ThemeList.vue
@@ -1,13 +1,13 @@
 <template>
   <dl class="theme-list">
     <template v-for="(theme, index) in themes">
-        <dt v-if="(theme.slug === activeSlug)" v-bind:class="`h3 active ${theme.slug}`" v-bind:key="`${theme.slug}-title`">
+        <dt v-if="(theme.slug === activeSlug)" v-bind:class="`h3 active invert ${theme.slug}`" v-bind:key="`${theme.slug}-title`">
           {{ theme.title }}
         </dt>
-        <dt v-else v-bind:class="`h3 ${theme.slug}`" v-bind:key="`${theme.slug}-title`">
+        <dt v-else v-bind:class="`h3 invert ${theme.slug}`" v-bind:key="`${theme.slug}-title`">
           <nuxt-link v-bind:to="theme.path">{{ theme.title }}</nuxt-link>
         </dt>
-        <dd v-bind:key="`${theme.slug}-description`" v-html="formattedBodies[index]"></dd>
+        <dd class="invert" v-bind:key="`${theme.slug}-description`" v-html="formattedBodies[index]"></dd>
     </template>
   </dl>
 </template>
@@ -33,9 +33,9 @@ export default {
 
 .theme-list {
   padding-left: 1rem;
-  padding-bottom: 10rem;
+  padding-bottom: 1rem;
   width: 50vw;
-  max-width: 12rem;
+  max-width: 10rem;
   color: var(--ui--text--invert);
 }
 .theme-list ::selection {
@@ -47,8 +47,8 @@ export default {
   top:0;
   bottom: 0;
   left:0;
-  width:50vw;
-  max-width: 14rem;
+  width: 50vw;
+  max-width: 16rem;
   background-image: var(--ui--left-gradient);
   z-index: -1;
 }
@@ -59,19 +59,36 @@ export default {
 .theme-list dd {
   margin: 0;
   padding: 0;
+  position: relative;
 }
 .theme-list dd + dt {
-  margin-top: 1rem;
+  margin-top: .5rem;
+}
+.theme-list dt {
+  opacity: .6;
+  transition: .5s opacity;
+}
+.theme-list dt:hover,
+.theme-list dt.active {
+  opacity: 1;
 }
 .theme-list dt:not(.active) + dd {
   display: none;
+}
+.theme-list dt::before {
+  content: '▾';
+  position: absolute;
+  right: .5rem;
+}
+.theme-list dt.active::before {
+  content: '';
 }
 .theme-list dt::after {
   content:'';
   display: block;
   height: .25rem;
-  width: 12rem;
-  margin: .2rem 0 1rem 0;
+  width: 6rem;
+  margin: .25rem 0 1rem 0;
 }
 .theme-list dt.too-dirty::after {
   background-image: var(--too-dirty--gradient);

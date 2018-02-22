bac278d134d2b2142a3ed5c7e1a8b94359367b40
Fedor Baart <fedor.baart@deltares.nl>
2018-02-22 22:49:39 +0100

    add video

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <theme-list v-bind:themes="themes" />
+  <div class="main-page">
+    <theme-list v-bind:themes="themes" />
+    <main>
+      <video src="https://s3-eu-west-1.amazonaws.com/deltares-opendata/wwa/movies/waterpromise.mp4" controls autoplay draggable @dragend="hide"></video>
+    </main>
+  </div>
 </template>
-
 <script>
 import events from '~/lib/events'
 import loadData from '~/lib/load-data'
@@ -15,6 +19,51 @@ export default {
   components: { ThemeList },
   mounted () {
     this.$events.$emit(events.enableGlobeNavigation)
+  },
+  methods: {
+    hide (evt) {
+      evt.srcElement.classList.add('hidden')
+      // slowly lower the volume
+      let timer = 0
+      let volume = 1
+      // from 1 to 0
+      for (var i = 0; i < 10; i++) {
+        // every so often
+        setTimeout(
+          () => {
+            evt.srcElement.volume = volume
+          },
+          timer
+        )
+        timer += 200
+        volume -= 0.1
+      }
+      // from here the globe should render the video
+    }
   }
 }
 </script>
+<style>
+  .main-page main {
+    pointer-events: none;
+    touch-action: none;
+    top: 0;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+  .main-page video {
+    pointer-events: initial;
+    touch-action: initial;
+    max-height: 30vh;
+    margin: 2rem;
+  }
+  .main-page video.hidden {
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 2s, opacity 0.5s linear;
+  }
+</style>

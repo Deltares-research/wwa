8078f7be53770e1170fec8cc79078c2d2de4b2f4
matthijs schaap <matthijs.schaap@deltares.nl>
2018-02-26 17:15:42 +0100

    added markdown to staticPage body

--- pages/_staticPage.vue
+++ pages/_staticPage.vue
@@ -5,8 +5,7 @@
     <picture v-for="image in images" v-bind:key="image.id">
       <img v-bind:src="`${image.imgixHost}${image.value.path}?w=640&q=65`"/>
     </picture>
-    <section>
-      {{body}}
+    <section v-html="htmlBody">
     </section>
   </article>
 </template>
@@ -14,14 +13,38 @@
 <script>
 import events from '~/lib/events'
 import loadData from '~/lib/load-data'
+import marked from 'marked'
+
+const renderer = new marked.Renderer()
+renderer.paragraphCount = 0 // Need to keep track of the number of paragraphs
+renderer.paragraph = function (content) {
+  const i = this.paragraphCount || 0
+  this.paragraphCount++
+  return `<p ${(!i) ? 'class="intro"' : ''}>${content}</p>`
+}
 
 export default {
   async asyncData (context) {
     const { title, body, images } = await loadData(context, context.params)
     return { title, body, images }
   },
+  computed: {
+    htmlBody () {
+      return this.customMarked(this.body)
+    }
+  },
   mounted () {
     this.$events.$emit(events.disableGlobeNavigation)
+  },
+  methods: {
+    customMarked (content) {
+      let formatted
+      if (content) {
+        renderer.paragraphCount = 0
+        formatted = marked(content, { renderer })
+      }
+      return formatted
+    }
   }
 }
 </script>

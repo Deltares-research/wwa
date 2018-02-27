29c75f118eecda5d0d7b521663a9b7a30a4d4f1a
Celine Kurpershoek <celinekurpershoek@gmail.com>
2018-02-27 14:04:34 +0100

    quick solution for a submit link; waiting for designs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <main>
     <nav class="link-menu">
-      <nuxt-link class="home h1" to="/" title="Go home"><span class="sr-only">Return to the homepage</span>World<br />Water<br />Atlas</nuxt-link>
-      <nuxt-link class="about h3" to="/about">About</nuxt-link>
+      <nuxt-link class="link-menu__item link-menu__item--home h1" to="/" title="Go home"><span class="sr-only">Return to the homepage</span>World<br />Water<br />Atlas</nuxt-link>
+      <nuxt-link class="link-menu__item link-menu__item--about h3" to="/about">About</nuxt-link>
+      <nuxt-link class="link-menu__item link-menu__item--submit h3" to="/submit-a-story-to-world-water-atlas">Submit a story</nuxt-link>
     </nav>
     <globe-component v-bind:is="GlobeComponent" class="globe-component" />
     <nuxt/>
@@ -46,31 +47,39 @@ main {
   top: 0;
   color: var(--ui--text--invert);
 }
-.link-menu a:not(.home) {
+
+.link-menu .link-menu__item {
   float: right;
-  padding: 1rem;
+  padding: 0 1rem .8rem;
   opacity: .6;
   transition: .5s opacity;
   font-weight: normal;
-}
-.link-menu a,
-.link-menu a:hover,
-.link-menu a:focus,
-.link-menu a:active {
+  margin-top: 1.7rem;
   text-decoration: none;
 }
-a.home {
-  display: block;
+
+.link-menu__item:hover,
+.link-menu__item:focus,
+.link-menu__item:active {
+  text-decoration: none;
+  opacity: 1;
+  color: var(--ui--text--invert);
+}
+
+.link-menu .link-menu__item--home {
   text-decoration: none;
   font-weight: normal;
   position: absolute;
-  padding: 1rem;
+  padding: 1.4rem 1rem;
   background-color: var(--ui--black--trans);
   z-index: 100;
+  margin-top: 0;
 }
 
-.link-menu a:hover {
-  opacity: 1;
-  color: var(--ui--text--invert);
+.link-menu .link-menu__item--submit {
+  background: white;
+  margin-top: .7rem;
+  padding-top: .8rem;
+  color: var(--ui--text);
 }
 </style>

010ef73d8ecc1ceeddd8a9913d1329381fcc26dd
Jurgen Beliën <jurgen@voorhoede.nl>
2018-03-06 11:22:05 +0100

    render tagline on home

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <theme-list v-bind:themes="themes" />
+  <div class="tagline invert" v-html="body"></div>
 </template>
 
 <script>
 import loadData from '~/lib/load-data'
-import ThemeList from '~/components/theme-list/ThemeList'
+import marked from 'marked'
+import home from '~/static/data/home.json'
 
 export default {
   async asyncData (context) {
@@ -17,10 +18,48 @@ export default {
 
     return { books, markers, themes: await themes }
   },
+  data () {
+    const body = marked(home.body)
+    return { body }
+  },
   mounted () {
     this.$store.commit('replaceFeatures', this.markers)
     this.$store.commit('enableInteraction')
-  },
-  components: { ThemeList }
+  }
 }
 </script>
+
+<style>
+@import "../components/colors/colors.css";
+
+.tagline {
+  position: absolute;
+  top: 5rem;
+  margin-top: 5vw;
+  width: 25vw;
+  min-width: 10rem;
+  max-width: 15rem;
+  padding: 1rem;
+}
+
+.tagline [href*='too-dirty'],
+.tagline [href*='too-much'],
+.tagline [href*='too-little'] {
+  display: inline-block;
+  padding-left: 1.25em;
+  background-repeat: no-repeat;
+  background-position: left center;
+  background-size: 1em;
+}
+.tagline [href*='too-dirty'] {
+  background-image: url('~/static/assets/too-dirty.png');
+}
+.tagline [href*='too-much'] {
+  background-image: url('~/static/assets/too-much.png');
+}
+
+.tagline [href*='too-little'] {
+  background-image: url('/static/assets/too-little.png');
+}
+
+</style>

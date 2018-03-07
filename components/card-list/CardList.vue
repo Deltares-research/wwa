282afa9519d49343ccb408d8fb2af1886ac0052c
Jurgen Beliën <jurgen@voorhoede.nl>
2018-03-07 15:11:34 +0100

    scroll list only when overflowing

--- components/card-list/CardList.vue
+++ components/card-list/CardList.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="card-list">
     <transition-group v-if="cards" class="card-list" name="slideUp" tag="ul" appear disappear :duration="3000">
-      <li v-for="(card, index) in cards" v-bind:key="card.slug" class="card-list__item">
+      <li v-for="card in cards"
+        v-bind:key="card.slug"
+        v-bind:id="card.slug"
+        data-list-item
+        class="card-list__item">
         <card-component
-          v-bind:delay="index * 100"
           v-bind:subtitle="(card.book) ? card.book.title : subtitle"
           v-bind:title="card.title"
           v-bind:slug="card.slug"
           v-bind:path="card.path"
-          v-bind:body="card.body"
-          v-bind:theme="card.theme"
-          v-bind:count="card.pageCount"
         />
       </li>
     </transition-group>
-  <button class="card-list__scroll-button" v-on:click="scroll()"><span class="sr-only">Scroll to next</span>‣</button>
+    <button v-bind:hidden="!nextListItem" class="card-list__scroll-button" v-on:click="scrollToNext()">
+      <span class="sr-only">Scroll to next</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="#fff" stroke-width="1.5" d="M7 2l11 10L7 23"/></svg>
+    </button>
   </div>
 
 </template>
@@ -23,8 +26,10 @@
 import CardComponent from '~/components/card-component/CardComponent'
 
 export default {
-  components: {
-    CardComponent
+  data () {
+    return {
+      nextListItem: undefined
+    }
   },
   props: {
     cards: Array,
@@ -35,14 +40,48 @@ export default {
       }
     }
   },
-  methods: {
-    scroll () {
-      const width = document.width || document.body.clientWidth
-      this.$el.querySelector('ul').scrollLeft = width
+  mounted () {
+    if ('IntersectionObserver' in window && 'scrollIntoView' in document.body) {
+      this.observeIntersectingChildren()
     }
-  }
+  },
+  methods: {
+    observeIntersectingChildren () {
+      const trackVisibility = (entries) => {
+        const lastListItemEntry = entries[entries.length - 1]
+        let nextListItem = this.nextListItem
+        if (lastListItemEntry.intersectionRatio === 1) {
+          nextListItem = false
+        } else {
+          entries.forEach((entry, index) => {
+            if (entry.intersectionRatio <= 1 && entry.intersectionRatio > 0) {
+              // lastVisibleElement = entry.target
+              nextListItem = entry.target
+            }
+          })
+        }
+        this.nextListItem = nextListItem
+      }
+      console.log()
+      const observer = new IntersectionObserver(trackVisibility, {
+        root: this.$el, // root element is card-list
+        rootMargin: '0% 0% 0% 0%',
+        thresholds: 1
+      })
+      const cardComponentsArray = [].slice.call(this.$el.querySelectorAll('[data-list-item]'))
+      cardComponentsArray.forEach(el => observer.observe(el))
+    },
+    scrollToNext () {
+      if (this.nextListItem) {
+        const left = this.nextListItem.offsetLeft
+        this.nextListItem.parentElement.scrollLeft = left
+      }
+    }
+  },
+  components: { CardComponent }
 }
 </script>
+
 <style>
 @import '../colors/colors.css';
 @import '../animations/animations.css';
@@ -76,8 +115,12 @@ export default {
   flex: 0 0 22rem;
   width: 22rem;
   margin: 0;
+  padding: 0;
   vertical-align: top;
   white-space: normal;
+}
+.card-list li:last-child {
+  margin-right: 1rem;
 }
 
 @media screen and (min-width: 72rem) {
@@ -88,15 +131,18 @@ export default {
 }
 
 .card-list__scroll-button {
+  border: none;
   position: absolute;
   top: 4rem;
   right: 1rem;
-  background-color: var(--ui--white);
   width: 2rem;
   height: 2rem;
   border-radius: 1rem;
+  background-color: var(--ui--black--trans);
   box-shadow: 2rem 2rem 4rem 4rem var(--ui--black--trans);
   z-index: 1;
+  font-size: 2rem;
+  text-align: center;
+  line-height: .25rem;
 }
-
 </style>

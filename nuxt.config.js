f29e9e69cfa8870cd80ce940f63b1c6479363da4
Celine Kurpershoek <celinekurpershoek@gmail.com>
2018-03-08 16:31:55 +0100

    🖖|

--- nuxt.config.js
+++ nuxt.config.js
@@ -105,7 +105,7 @@ module.exports = {
       { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
       { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
-      { rel: 'icon', type: 'image/png', sizes: '192x192',  href: '/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }

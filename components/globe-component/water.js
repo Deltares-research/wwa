d740857a588fcd396f39850a071734f7fb277ba4
Jan Willem Tulp <janwillemtulp@tulpinteractive.com>
2018-03-08 14:43:03 +0100

    changed expensive nice looking shader water to single color water

--- components/globe-component/water.js
+++ components/globe-component/water.js
@@ -4,7 +4,7 @@ import { GLOBE_RADIUS } from './constants'
 
 class Water {
   constructor () {
-    this.geometry = new THREE.SphereGeometry(GLOBE_RADIUS - 0.02, 32, 64)
+    this.geometry = new THREE.SphereGeometry(GLOBE_RADIUS - 0.02, 64, 64)
 
     this.uniforms = {
       time: { value: 0 }
@@ -12,11 +12,18 @@ class Water {
 
     // https://shaderfrog.com/app/view/2113
 
-    this.material = new THREE.ShaderMaterial({
-      uniforms: this.uniforms,
-      vertexShader: require('./glsl/water.vert.glsl'),
-      fragmentShader: require('./glsl/water.frag.glsl'),
-      transparent: true
+    // this.material = new THREE.ShaderMaterial({
+    //   uniforms: this.uniforms,
+    //   vertexShader: require('./glsl/water.vert.glsl'),
+    //   fragmentShader: require('./glsl/water.frag.glsl'),
+    //   transparent: true
+    // })
+
+    this.material = new THREE.MeshBasicMaterial({
+      color: 0x003355,
+      // map: new THREE.TextureLoader().load('assets/water-blue-ocean.jpg'),
+      transparent: true,
+      opacity: 0.9
     })
 
     this.mesh = new THREE.Mesh(this.geometry, this.material)

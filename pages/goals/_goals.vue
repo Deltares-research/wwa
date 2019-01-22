00aad7838656e5cca868713622cb7078b8debe75
Stefan Kool <stefankool@MacBook-Pro-van-Stefan.local>
2019-01-22 10:54:24 +0100

    pr review changes, background icons and generating routes

--- pages/goals/_goals.vue
+++ pages/goals/_goals.vue
@@ -7,7 +7,8 @@
         <ul class="list--inline goal-filter">
           <li v-for="link in goals" :key="link.slug">
             <nuxt-link
-              :class="`sdg-tag sdg-tag--filter sdg-tag--large sdg-tag--${link.slug.slice(0,2)}`"
+              class="sdg-tag sdg-tag--filter sdg-tag--large"
+              :style="{ backgroundImage: `url('assets/E_SDG-goals_icons-individual-rgb-${link.slug.slice(0,2)}.png')` }"
               :to="link.path"
               :title="link.title"
               >

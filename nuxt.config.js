f579f2fd2d241d5eabc900f097d642007539ff9d
Stefan Kool <stefankool@MacBook-Pro-van-Stefan.local>
2019-01-22 13:58:30 +0100

    refactor merge conflict with plugins in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,7 +113,7 @@ module.exports = {
   plugins: [
     { src: '~/plugins/smoothscroll', ssr: false },
     { src: '~/plugins/vue-portal', ssr: true },
-    { src: '~plugins/ga.js', ssr: false }
+    { src: '~/plugins/ga.js', ssr: false }
   ],
 
   // include routerbase

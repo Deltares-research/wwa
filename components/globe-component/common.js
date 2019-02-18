7e72e1095cff6eb0c45090ef555ff02c8f94cbb4
Jan Willem Tulp <janwillemtulp@tulpinteractive.com>
2019-02-18 17:10:29 +0100

    putting all the avaters that belong to the same cluster at the same mean location

--- components/globe-component/common.js
+++ components/globe-component/common.js
@@ -34,7 +34,39 @@ export function cartesian2polar (x, y, z) {
   return { r, theta, phi }
 }
 
+/**
+ * Converts degrees to radians.
+ * @param {Number} deg
+ */
 export const deg2rad = deg => deg * (Math.PI / 180)
+
+/**
+ * A function that takes any two lat/lon points and returns a scalar value describing the great-circle distance between them.
+ * https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+ * @param {Array} p0 first coordinate (2-dimensional array, as [lon, lat])
+ * @param {Array} p1 second coordinate (2-dimensional array, as [lon, lat])
+ */
+export const greatCircleDistance = (p0, p1) => {
+  const lat1 = p0[1]
+  const lon1 = p0[0]
+  const lat2 = p1[1]
+  const lon2 = p1[0]
+
+  var R = 6371 // Radius of the earth in km
+
+  var dLat = deg2rad(lat2 - lat1)
+  var dLon = deg2rad(lon2 - lon1)
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  var d = R * c // Distance in km
+
+  return d
+}
 
 /**
  * Convert from latitude to theta (latitude in radians)

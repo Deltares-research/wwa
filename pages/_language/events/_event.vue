95135ca4e4fb2dd3463804bc7e841559f9c6032f
Selwyn <talk@selwyn.cc>
2020-06-03 11:47:05 +0200

    Add initial language navigation

--- pages/_language/events/_event.vue
+++ pages/_language/events/_event.vue
@@ -1,5 +1,21 @@
 <template>
-  <h2 class="event__title">{{ event.title }}</h2>
+  <main class="event">
+    <h2 class="event__title">{{ event.title }}</h2>
+    <nav>
+      <ul>
+        <li v-for="{ locale } in event._allTitleLocales" :key="locale">
+          <a
+            rel="alternate"
+            :hreflang="locale"
+            :lang="locale"
+            :href="`/${locale}/events/${params.event}`"
+          >
+            {{ locale }}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
 
 <script>
@@ -7,24 +23,36 @@
 
   export default {
     layout: 'empty',
-    asyncData({ params }) {
-      return fetchContent(`
+    async asyncData({ params }) {
+      const query = `
         {
           event(locale: ${params.language}, filter: { slug: { eq: "${params.event}" } }) {
             slug
             title
+
+            _allTitleLocales {
+              locale
+            }
           }
         }
-      `)
+      `;
+
+      return {
+        ...await fetchContent(query),
+        params,
+      }
     }
   }
 </script>
 
 <style>
+  .event {
+    color: white;
+  }
+
   .event__title {
     display: flex;
     justify-content: center;
-    color: white;
     font-size: 4rem;
   }
 </style>

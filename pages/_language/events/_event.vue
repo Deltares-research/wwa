21591171fc290fe9a6764a760581571f0d80cb30
Selwyn <talk@selwyn.cc>
2020-06-03 10:33:31 +0200

    Setup initial event page language structure

---
+++ pages/_language/events/_event.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2 class="event__title">{{ event.title }}</h2>
+</template>
+
+<script>
+  import fetchContent from '~/lib/fetch-content';
+
+  export default {
+    layout: 'empty',
+    asyncData({ params }) {
+      return fetchContent(`
+        {
+          event(locale: ${params.language}, filter: { slug: { eq: "${params.event}" } }) {
+            slug
+            title
+          }
+        }
+      `)
+    }
+  }
+</script>
+
+<style>
+  .event__title {
+    display: flex;
+    justify-content: center;
+    color: white;
+    font-size: 4rem;
+  }
+</style>

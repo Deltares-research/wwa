f2bdd29ad32e6468b6bdffb5a50d2e6ace229ffc
Selwyn <talk@selwyn.cc>
2020-06-17 15:16:38 +0200

    Add links to events page

--- pages/events.vue
+++ pages/events.vue
@@ -4,7 +4,7 @@
       v-for="event in [...allInternalEvents, ...allExternalEvents]"
       :key="event.id"
     >
-      <li>
+      <li class="event-preview">
         Date: {{ event.startDate }}
         Location: {{ event.location }}
         <img
@@ -12,6 +12,11 @@
           alt=""
         >
         Name: {{ event.name }}
+
+        <div class="event-preview__link">
+          <a v-if="event.url" :href="event.url">{{ event.url }}</a>
+          <nuxt-link v-else :to="`/en/events/${event.slug}`">Go to event page</nuxt-link>
+        </div>
       </li>
     </ol>
   </div>
@@ -29,6 +34,7 @@
             id
             startDate
             location
+            url
             image {
               url
             }
@@ -37,6 +43,7 @@
           allInternalEvents {
             name
             id
+            slug
             startDate
             location
             image {
@@ -54,4 +61,11 @@
 </script>
 
 <style>
+  .event-preview {
+    margin: 2rem;
+  }
+
+  .event-preview__link {
+    font-weight: bold;
+  }
 </style>

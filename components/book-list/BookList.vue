18f3f5a7dd8970f47d90d57887b927b9e89402a6
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2020-06-17 11:14:12 +0200

    Update design of book list pages

--- components/book-list/BookList.vue
+++ components/book-list/BookList.vue
@@ -4,6 +4,7 @@
       v-for="book in books"
       :key="book.slug"
       :id="book.slug"
+      class="book-list__item"
     >
       <h2 class="book-list__book-title">
         <nuxt-link
@@ -13,32 +14,23 @@
           {{ book.title }}
         </nuxt-link>
       </h2>
-      <slot
-        v-if="hasSlotContent"
-        :chapters="book.chapters"
-        :limit="bookLimitMap[book.slug]"
-      />
-      <chapter-list
-        v-else
-        :chapters="book.chapters"
-        :limit="bookLimitMap[book.slug]"
-      />
+      <div class="book-list__content">
+        <slot
+          v-if="hasSlotContent"
+          :chapters="book.chapters"
+          :limit="bookLimitMap[book.slug]"
+        />
+        <chapter-list
+          v-else
+          :chapters="book.chapters"
+          :limit="bookLimitMap[book.slug]"
+        />
+      </div>
       <nuxt-link
-        class="book-list__more"
+        class="book-list__more-link"
         :to="book.path"
       >
-        <span class="book-list__more-content">
-          {{ book.title }} <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            class="book-list__more-chevron"
-          ><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" /><path
-            d="M0 0h24v24H0z"
-            fill="none"
-          /></svg>
-        </span>
+        View more {{ book.title }}
       </nuxt-link>
     </li>
   </ul>
@@ -68,19 +60,27 @@ export default {
 </script>
 
 <style>
-.book-list__heading {
-  margin: 0;
-}
-
 .book-list {
   list-style: none;
   padding: 0px;
 }
 
-.book-list__book-title {
+.book-list__item {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 3rem;
+}
+
+.book-list__item:last-child {
   margin-bottom: 1rem;
-  font-size: 1.25rem;
-  line-height: 2.1875rem;
+}
+
+.book-list__book-title {
+  flex: 0 0 100%;
+  margin-bottom: 1rem;
+  font-size: 1.5rem;
+  line-height: 1.1;
   font-weight: 500;
 }
 
@@ -93,51 +93,29 @@ export default {
   text-decoration: underline;
 }
 
-.book-list__more {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  border: none;
-  text-decoration: none;
-  color: var(--ui-white);
-  background-color: transparent;
-  position: relative;
-  padding: 0;
-  z-index: 0;
-  margin-top: calc(50px - 1rem);
-  margin-bottom: calc(50px - 1rem);
+.book-list__content {
+  flex: 0 0 100%;
 }
 
-.book-list__more-content{
-  background-color: var(--ui--blue--bg);
-  text-transform: uppercase;
-  font-size: 0.875rem;
+.book-list__more-link {
+  display: inline-block;
+  padding: 10px 15px;
+  background: var(--ui--blue--light);
+  font-size: 1rem;
   font-weight: bold;
-  padding: 0.5rem 1rem 0.5rem 1.5rem ;
-  border-radius: 2em;
-  cursor: pointer;
-  display: inline-flex;
-  align-items: center;
+  text-decoration: none;
+  color: var(--ui--blue);
+  border-radius: 5px;
 }
 
-.book-list__more-chevron {
-  fill: white;
-}
-
-.book-list__more:before {
-  content: '';
-  display: block;
-  width: 100%;
-  height: 2px;
-  position: absolute;
-  background-color: var(--ui--blue--bg);
-  top: calc(50% - 2px);
-  z-index: -1;
+.book-list__more-link:hover,
+.book-list__more-link:focus {
+  color: var(--ui--blue);
 }
 
 @media (min-width: 600px) {
   .book-list__book-title {
-    font-size: 1.5rem;
+    font-size: 2rem;
     line-height: 2.25rem;
   }
 

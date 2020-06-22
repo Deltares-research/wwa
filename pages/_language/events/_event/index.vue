348408b8df01f11d326662d171f95385744773da
Selwyn <talk@selwyn.cc>
2020-06-22 11:52:37 +0200

    Add initial styling for content text block

--- pages/_language/events/_event/index.vue
+++ pages/_language/events/_event/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <header>
+  <div class="event">
+    <header class="event__layout">
       <event-header
         :name="internalEvent.name"
         :location="internalEvent.location"
@@ -11,11 +11,15 @@
       <event-banner v-bind="internalEvent" />
     </header>
 
-    <main class="event__body">
-      <section
+    <main>
+      <div
         v-for="section in internalEvent.sections"
         :key="section.id"
         class="event-section"
+        :class="{
+          'event-section--background-black': section.backgroundColor === 'black',
+          'event-section--background-blue': section.backgroundColor === 'blue',
+        }"
       >
         <img
           class="event-section__divider"
@@ -24,46 +28,65 @@
           height="35"
           alt=""
         >
-        <h3 class="event-section__title">
-          {{ section.title }}
-        </h3>
-        <img
-          v-if="section.image"
-          class="event-section__image"
-          :src="`${section.image.url}?auto=compress`"
-          :alt="section.image.alt"
-          width="320"
-        >
-        <span
-          class="event-section__body"
-          v-html="section.body"
-        />
-        <ul class="event__relevant-chapters">
-          <li
-            class="chapter-preview"
-            v-for="chapter in section.chapters"
-            :key="chapter.slug"
+        <section class="event__layout event__layout--padded event-section__body">
+          <img
+            v-if="section.waveMarker"
+            class="event-section__divider"
+            src="/event-section-wave.svg"
+            width="111"
+            height="35"
+            alt=""
           >
-            <nuxt-link
-              class="chapter-preview__link"
-              :to="chapter.slug"
-              append
+          <h3
+            class="event-section__title"
+            :class="{
+              'event-section__title--orange': section.titleColor === 'orange',
+              'event-section__title--blue': section.titleColor === 'blue',
+            }"
+          >
+            {{ section.title }}
+          </h3>
+          <img
+            v-if="section.image"
+            class="event-section__image"
+            :src="`${section.image.url}?auto=compress`"
+            :alt="section.image.alt"
+            width="320"
+          >
+          <span
+            class="event-section__copy"
+            v-html="section.body"
+          />
+          <ul
+            v-if="section.chapters.length"
+            class="event__relevant-chapters"
+          >
+            <li
+              class="chapter-preview"
+              v-for="chapter in section.chapters"
+              :key="chapter.slug"
             >
-              <img
-                class="chapter-preview__image"
-                v-if="chapter.cover"
-                width="200"
-                height="200"
-                :src="`${chapter.cover.url}?auto=compress&w=400`"
-                alt=""
+              <nuxt-link
+                class="chapter-preview__link"
+                :to="chapter.slug"
+                append
               >
-              <h4 class="chapter-preview__title">
-                {{ chapter.title }}
-              </h4>
-            </nuxt-link>
-          </li>
-        </ul>
-      </section>
+                <img
+                  class="chapter-preview__image"
+                  v-if="chapter.cover"
+                  width="200"
+                  height="200"
+                  :src="`${chapter.cover.url}?auto=compress&w=400`"
+                  alt=""
+                >
+                <h4 class="chapter-preview__title">
+                  {{ chapter.title }}
+                </h4>
+              </nuxt-link>
+            </li>
+          </ul>
+        </section>
+      </div>
       <section>
         <ul
           v-for="speaker in internalEvent.speakers"
@@ -136,7 +159,10 @@
             sections {
               id
               title
+              titleColor
               body(markdown: true)
+              backgroundColor
+              waveMarker
               image {
                 url
                 alt
@@ -172,12 +198,35 @@
 </script>
 
 <style>
-  .event__body {
-    padding: 0.6rem;
+  .event__layout {
+    max-width: 60rem;
+    margin-right: auto;
+    margin-left: auto;
   }
 
-  .event-section {
-    margin-bottom: 2rem;
+  .event__layout--padded {
+    padding-right: 1.2rem;
+    padding-left: 1.2rem;
+  }
+
+  @media (min-width: 38rem) {
+    .event__layout--padded {
+      padding-right: 2.4rem;
+      padding-left: 2.4rem;
+    }
+  }
+
+  .event-section--background-black {
+    background-color: var(--black);
+  }
+
+  .event-section--background-blue {
+    background-color: var(--primary-blue);
+  }
+
+  .event-section__body {
+    padding-top: 3.4rem;
+    padding-bottom: 3.4rem;
   }
 
   .event-section__divider {
@@ -186,19 +235,46 @@
 
   .event-section__title {
     font-size: 2rem;
+    font-weight: 900;
     margin-bottom: 1rem;
+    max-width: 30rem;
+  }
+
+  @media (min-width: 38rem) {
+    .event-section__title {
+      font-size: 3.75rem;
+    }
+  }
+
+  .event-section__title--orange {
+    color: var(--orange);
+  }
+
+  .event-section__title--blue {
+    color: var(--tertiary-blue);
   }
 
   .event-section__image {
     margin-bottom: 1rem;
   }
 
-  .event-section__body {
+  .event-section__copy {
+    display: block;
+    column-count: 2;
+    column-width: 25rem;
+    column-gap: 1.2rem;
     margin-bottom: 1.4rem;
+  }
+
+  .event-section__copy p {
+    display: inline-block;
+    margin-bottom: 1rem;
+    max-width: 30rem;
   }
 
   .event__relevant-chapters {
     display: flex;
+    overflow-x: scroll;
   }
 
   .chapter-preview {

6978fd73add5780e66a619f164c8034dadf84a78
Selwyn <talk@selwyn.cc>
2020-06-24 17:52:20 +0200

    WIP

---
+++ components/event-block/EventBlockText.vue
@@ -0,0 +1,76 @@
+<template functional>
+  <div>
+    <img
+      v-if="props.showWaveMarker"
+      src="/event-title-wave.svg"
+      width="111"
+      height="35"
+      alt=""
+    >
+    <h3
+      class="event-block-text__title"
+      :class="{
+        'event-block-text__title--orange': props.titleColor === 'orange',
+        'event-block-text__title--blue': props.titleColor === 'blue',
+      }"
+    >
+      {{ props.title }}
+    </h3>
+    <div
+      class="event-block-text__copy"
+      v-html="props.body"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      showWaveMarker: Boolean,
+      title: String,
+      titleColor: String,
+      body: String,
+    },
+  };
+</script>
+
+<style>
+  .event-block-text__title {
+    font-size: 2rem;
+    font-weight: 900;
+    margin-bottom: 1rem;
+    max-width: 30rem;
+  }
+
+  .event-block-text__title--orange {
+    color: var(--orange);
+  }
+
+  .event-block-text__title--blue {
+    color: var(--tertiary-blue);
+  }
+
+  @media (--sm-viewport) {
+    .event-block-text__title {
+      font-size: 5rem;
+      font-weight: 900;
+    }
+
+    .event-block-text__image {
+      margin-bottom: 1rem;
+    }
+
+    .event-block-text__copy {
+      display: block;
+      column-count: 2;
+      column-width: 25rem;
+      column-gap: 1.2rem;
+    }
+
+    .event-block-text__copy p {
+      display: inline-block;
+      margin-bottom: 1rem;
+      max-width: 30rem;
+    }
+  }
+</style>
---
+++ components/event-block/EventBlockTextMedia.vue
@@ -0,0 +1,76 @@
+<template functional>
+  <div>
+    <img
+      v-if="props.showWaveMarker"
+      src="/event-title-wave.svg"
+      width="111"
+      height="35"
+      alt=""
+    >
+    <h3 class="event-block-text-media__title">
+      {{ props.title }}
+    </h3>
+    <picture v-if="props.image">
+      <source
+        :srcset="props.image.portrait.srcSet"
+        :sizes="props.image.portrait.sizes"
+        media="(min-width: 800px)"
+      >
+      <source
+        :srcset="props.image.landscape.srcSet"
+        :sizes="props.image.landscape.sizes"
+        media="(max-width: 800px)"
+      >
+      <img
+        class="event-section__image"
+        :src="props.image.portrait.src"
+        :width="props.image.portrait.width"
+        :alt="props.image.alt"
+      >
+    </picture>
+    <div
+      class="event-block-text-media__copy"
+      v-html="props.body"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      showWaveMarker: Boolean,
+      title: String,
+      image: {
+        portrait: Object,
+        landscape: Object,
+      },
+      body: String,
+    },
+  };
+</script>
+
+<style>
+  .event-block-text-media__title {
+    font-size: 2rem;
+    font-weight: 900;
+    margin-bottom: 1rem;
+    max-width: 30rem;
+  }
+
+  @media (--sm-viewport) {
+    .event-block-text-media__title {
+      font-size: 3.75rem;
+      font-weight: 900;
+    }
+
+    .event-block-text-media__image {
+      margin-bottom: 1rem;
+    }
+
+    .event-block-text-media__copy {
+    }
+
+    .event-block-text-media__copy p {
+    }
+  }
+</style>

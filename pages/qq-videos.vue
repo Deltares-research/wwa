dc589c2286ab1f7300eedcd5fbf1ba2073241d8a
Jasper Moelker <j.b.moelker@activeids.nl>
2020-06-26 14:18:25 +0200

    feat: qq video embed (working)

---
+++ pages/qq-videos.vue
@@ -0,0 +1,17 @@
+/* eslint-disable vue/no-parsing-error */
+<template>
+  <div style="padding:1rem">
+    <h1>qq videos</h1>
+    <a href="https://v.qq.com/x/page/a0787rl8q8p.html">demo video</a>
+
+    <h2>Iframe</h2>
+    <iframe
+      src="https://v.qq.com/txp/iframe/player.html?vid=a0787rl8q8p"
+      allowFullScreen="true"
+      frameborder="1"
+      width="500px"
+      height="300px"
+      style="background:white;"
+    />
+  </div>
+</template>

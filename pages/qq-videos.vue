0f5759061af0ffc4e74a0e6d5cbf801fedc570be
Jasper Moelker <j.b.moelker@activeids.nl>
2020-06-29 16:12:44 +0200

    Use video provided by China team

--- pages/qq-videos.vue
+++ pages/qq-videos.vue
@@ -2,11 +2,11 @@
 <template>
   <div style="padding:1rem">
     <h1>qq videos</h1>
-    <a href="https://v.qq.com/x/page/a0787rl8q8p.html">demo video</a>
+    <a href="https://v.qq.com/x/page/n31074rhp1h.html">demo video</a>
 
     <h2>Iframe</h2>
     <iframe
-      src="https://v.qq.com/txp/iframe/player.html?vid=a0787rl8q8p"
+      src="https://v.qq.com/txp/iframe/player.html?vid=n31074rhp1h"
       allowFullScreen="true"
       frameborder="1"
       width="500px"

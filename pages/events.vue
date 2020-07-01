e4fa3e6e910812877782bfc5de4611eecf8872aa
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2020-07-01 16:39:29 +0200

    Set up layout for event page

--- pages/events.vue
+++ pages/events.vue
@@ -1,36 +1,46 @@
 <template>
   <div>
-    <ol
-      v-for="event in [...allInternalEvents, ...allExternalEvents]"
-      :key="event.id"
-    >
-      <li class="event-preview">
-        {{ event.startDate }}
-        <img
-          :src="`${event.image.url}?auto=compress&w=400`"
-          alt=""
-        >
-        {{ event.name }}
+    <main-menu variant="dark" />
 
-        <div class="event-preview__link">
-          <a
-            v-if="event.url"
-            :href="event.url"
-          >{{ event.url }}</a>
-          <nuxt-link
-            v-else
-            :to="`/en/events/${event.slug}`"
-          >
-            Go to event page
-          </nuxt-link>
-        </div>
-      </li>
-    </ol>
+    <main class="layout-section">
+      <div class="layout-section__container">
+        <ol
+          v-for="event in [...allInternalEvents, ...allExternalEvents]"
+          :key="event.id"
+        >
+          <li class="event-preview">
+            {{ event.startDate }}
+            <img
+              :src="`${event.image.url}?auto=compress&w=400`"
+              alt=""
+            >
+            {{ event.name }}
+
+            <div class="event-preview__link">
+              <a
+                v-if="event.url"
+                :href="event.url"
+              >{{ event.url }}</a>
+              <nuxt-link
+                v-else
+                :to="`/en/events/${event.slug}`"
+              >
+                Go to event page
+              </nuxt-link>
+            </div>
+          </li>
+        </ol>
+      </div>
+    </main>
+
+    <app-footer />
   </div>
 </template>
 
 <script>
   import fetchContent from '~/lib/fetch-content';
+  import MainMenu from '~/components/main-menu/MainMenu';
+  import AppFooter from '~/components/app-footer/AppFooter';
 
   export default {
     async asyncData() {
@@ -62,6 +72,7 @@
         ...await fetchContent(query),
       };
     },
+    components: { MainMenu, AppFooter },
   };
 </script>
 

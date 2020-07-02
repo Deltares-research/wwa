acc887ad410180229ad59cb79d5a8cf0c856a534
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2020-07-02 14:04:58 +0200

    Set up desktop design

--- components/event-overview-list/EventOverviewList.vue
+++ components/event-overview-list/EventOverviewList.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="event-overview-list">
     <ul
       role="tablist"
       class="event-overview-list__tabs list--inline"
@@ -52,45 +52,48 @@
           :key="event.id"
           class="event-overview-list__list-item"
         >
-          <h3
-            v-html="event.name"
-            class="event-overview-list__name"
-          />
+          <div class="event-overview-list__list-item-header">
+            <h3
+              v-html="event.name"
+              class="event-overview-list__name"
+            />
 
-          <lazy-media>
+            <p class="event-overview-list__summary">
+              {{ event.summary }}
+            </p>
+
+            <a
+              v-if="event.url"
+              :href="event.url"
+              class="event-overview-list__link"
+            >
+              {{ event.urlLabel }}
+            </a>
+            <nuxt-link
+              v-else
+              :to="`/en/events/${event.slug}`"
+              class="event-overview-list__link event-overview-list__link--internal"
+            >
+              Go to event page
+            </nuxt-link>
+          </div>
+
+          <lazy-media class="event-overview-list__list-item-image">
             <img
               :src="`${event.image.url}?auto=compress&w=600`"
               alt=""
-              class="event-overview-list__image"
             >
           </lazy-media>
 
-          <p class="event-overview-list__date">
-            {{ event.displayDate }}
-          </p>
+          <div class="event-overview-list__list-item-footer">
+            <p class="event-overview-list__date">
+              {{ event.displayDate }}
+            </p>
 
-          <p class="event-overview-list__location">
-            {{ event.location }}
-          </p>
-
-          <p class="event-overview-list__summary">
-            {{ event.summary }}
-          </p>
-
-          <a
-            v-if="event.url"
-            :href="event.url"
-            class="event-overview-list__link"
-          >
-            {{ event.urlLabel }}
-          </a>
-          <nuxt-link
-            v-else
-            :to="`/en/events/${event.slug}`"
-            class="event-overview-list__link event-overview-list__link--internal"
-          >
-            Go to event page
-          </nuxt-link>
+            <p class="event-overview-list__location">
+              {{ event.location }}
+            </p>
+          </div>
         </li>
       </ol>
     </article>
@@ -107,45 +110,48 @@
           :key="event.id"
           class="event-overview-list__list-item"
         >
-          <h3
-            v-html="event.name"
-            class="event-overview-list__name"
-          />
+          <div class="event-overview-list__list-item-header">
+            <h3
+              v-html="event.name"
+              class="event-overview-list__name"
+            />
 
-          <lazy-media>
+            <p class="event-overview-list__summary">
+              {{ event.summary }}
+            </p>
+
+            <a
+              v-if="event.url"
+              :href="event.url"
+              class="event-overview-list__link"
+            >
+              {{ event.urlLabel }}
+            </a>
+            <nuxt-link
+              v-else
+              :to="`/en/events/${event.slug}`"
+              class="event-overview-list__link event-overview-list__link--internal"
+            >
+              Go to event page
+            </nuxt-link>
+          </div>
+
+          <lazy-media class="event-overview-list__list-item-image">
             <img
               :src="`${event.image.url}?auto=compress&w=600`"
               alt=""
-              class="event-overview-list__image"
             >
           </lazy-media>
 
-          <p class="event-overview-list__date">
-            {{ event.displayDate }}
-          </p>
+          <div class="event-overview-list__list-item-footer">
+            <p class="event-overview-list__date">
+              {{ event.displayDate }}
+            </p>
 
-          <p class="event-overview-list__location">
-            {{ event.location }}
-          </p>
-
-          <p class="event-overview-list__summary">
-            {{ event.summary }}
-          </p>
-
-          <a
-            v-if="event.url"
-            :href="event.url"
-            class="event-overview-list__link"
-          >
-            {{ event.urlLabel }}
-          </a>
-          <nuxt-link
-            v-else
-            :to="`/en/events/${event.slug}`"
-            class="event-overview-list__link event-overview-list__link--internal"
-          >
-            Go to event page
-          </nuxt-link>
+            <p class="event-overview-list__location">
+              {{ event.location }}
+            </p>
+          </div>
         </li>
       </ol>
     </article>
@@ -194,6 +200,16 @@
 </script>
 
 <style>
+  .event-overview-list {
+    margin-bottom: 1.5rem;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list {
+      margin-bottom: 3rem;
+    }
+  }
+
   .event-overview-list__tabs {
     margin-bottom: 1.5rem;
     font-size: 1.125rem;
@@ -227,6 +243,14 @@
     border-bottom: 1px solid var(--black-tertiary);
   }
 
+  @media (--md-viewport) {
+    .event-overview-list__list-item {
+      flex-direction: row;
+      justify-content: space-between;
+      margin-top: 2.5rem;
+    }
+  }
+
   .event-overview-list__list-item:last-child {
     padding-bottom: 0;
     border-bottom: none;
@@ -236,58 +260,82 @@
     margin-top: 0;
   }
 
+  .event-overview-list__list-item-header {
+    order: 3;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__list-item-header {
+      order: 2;
+      flex: 1 1 auto;
+    }
+  }
+
+  .event-overview-list__list-item-image {
+    order: 2;
+    margin-bottom: .5rem;
+  }
+
+  .event-overview-list__list-item-image img {
+    width: 100%;
+    height: 160px;
+    object-fit: cover;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__list-item-image {
+      order: 3;
+      flex: 0 0 290px;
+      margin-left: 2rem;
+      margin-bottom: 0;
+    }
+
+    .event-overview-list__list-item-image img {
+      height: 197px;
+    }
+  }
+
+  .event-overview-list__list-item-footer {
+    order: 1;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__list-item-footer {
+      flex: 0 0 20%;
+      margin-right: 2rem;
+    }
+  }
+
   .event-overview-list__name {
-    order: 4;
     margin-bottom: .25rem;
     font-size: 1.25rem;
+    line-height: 1;
     color: var(--orange);
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__name {
+      margin-bottom: .5rem;
+      font-size: 2rem;
+    }
   }
 
   .event-overview-list__name em {
     font-style: normal;
   }
 
-  .event-overview-list__image {
-    order: 3;
-    margin-bottom: .5rem;
-    width: 100%;
-    height: 160px;
-    object-fit: cover;
-  }
-
-  .event-overview-list__date {
-    order: 1;
-    font-size: 1.5rem;
-    font-weight: bold;
-    line-height: 1.2;
-  }
-
-  .event-overview-list__location {
-    order: 2;
-    display: flex;
-    align-items: center;
-    margin-bottom: .25rem;
-    font-size: 0.875rem;
-    font-weight: bold;
-  }
-
-  .event-overview-list__location:before {
-    content: '';
-    display: block;
-    margin-right: .4rem;
-    width: 14px;
-    height: 17px;
-    background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 14 17' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg opacity='.5' stroke='%23fff' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M13 7.1C13 12 7 16 7 16s-6-4-6-8.9c0-1.6.6-3.2 1.8-4.3a6 6 0 018.4 0A6.2 6.2 0 0113 7.1z'/%3E%3Cpath d='M7 9.2c1.1 0 2-1 2-2A2 2 0 007 5a2 2 0 00-2 2c0 1.2.9 2 2 2z'/%3E%3C/g%3E%3C/svg%3E");
-  }
-
   .event-overview-list__summary {
-    order: 5;
     margin-bottom: .5rem;
     line-height: 1.2;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__summary {
+      margin-bottom: .75rem;
+    }
   }
 
   .event-overview-list__link {
-    order: 6;
     font-weight: bold;
     text-decoration: none;
     color: var(--blue-tertiary);
@@ -311,5 +359,41 @@
     width: 14px;
     height: 10px;
     background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 14 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1 4.7h12M9.5 1L13 4.7 9.5 8.5' stroke='%2368D4F5' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
+  }
+
+  .event-overview-list__date {
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 1.2;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__date {
+      margin-bottom: .4rem;
+      line-height: 1;
+    }
+  }
+
+  .event-overview-list__location {
+    display: flex;
+    align-items: center;
+    margin-bottom: .25rem;
+    font-size: 0.875rem;
+    font-weight: bold;
+  }
+
+  @media (--md-viewport) {
+    .event-overview-list__location {
+      font-size: 1rem;
+    }
+  }
+
+  .event-overview-list__location:before {
+    content: '';
+    display: block;
+    margin-right: .4rem;
+    width: 14px;
+    height: 17px;
+    background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 14 17' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg opacity='.5' stroke='%23fff' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M13 7.1C13 12 7 16 7 16s-6-4-6-8.9c0-1.6.6-3.2 1.8-4.3a6 6 0 018.4 0A6.2 6.2 0 0113 7.1z'/%3E%3Cpath d='M7 9.2c1.1 0 2-1 2-2A2 2 0 007 5a2 2 0 00-2 2c0 1.2.9 2 2 2z'/%3E%3C/g%3E%3C/svg%3E");
   }
 </style>

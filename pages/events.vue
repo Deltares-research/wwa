0142ffbdcc432ca7713266f15fee2c4980f284c7
BasVoorhoede <66305989+BasVoorhoede@users.noreply.github.com>
2020-07-03 11:49:58 +0200

    Add event globe with icons (#193)
    * Set up event page structure for globe
    
    * Put all events as markers on the globe
    
    * Add new icon as marker on globe
    
    * Remove unnecessary code
    
    * Use globe layout for events overview page
    
    * Commit event marker icon
    
    Co-authored-by: Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>

--- pages/events.vue
+++ pages/events.vue
@@ -1,51 +1,46 @@
 <template>
-  <div>
-    <main-menu variant="dark" />
+  <div class="layout-section layout-section--events">
+    <div class="layout-section__container">
+      <ol
+        v-for="event in [...allInternalEvents, ...allExternalEvents]"
+        :key="event.id"
+      >
+        <li class="event-preview">
+          {{ event.startDate }}
+          <img
+            :src="`${event.image.url}?auto=compress&w=400`"
+            alt=""
+          >
+          {{ event.name }}
 
-    <main class="layout-section">
-      <div class="layout-section__container">
-        <ol
-          v-for="event in [...allInternalEvents, ...allExternalEvents]"
-          :key="event.id"
-        >
-          <li class="event-preview">
-            {{ event.startDate }}
-            <img
-              :src="`${event.image.url}?auto=compress&w=400`"
-              alt=""
+          <div class="event-preview__link">
+            <a
+              v-if="event.url"
+              :href="event.url"
+            >{{ event.url }}</a>
+            <nuxt-link
+              v-else
+              :to="`/en/events/${event.slug}`"
             >
-            {{ event.name }}
-
-            <div class="event-preview__link">
-              <a
-                v-if="event.url"
-                :href="event.url"
-              >{{ event.url }}</a>
-              <nuxt-link
-                v-else
-                :to="`/en/events/${event.slug}`"
-              >
-                Go to event page
-              </nuxt-link>
-            </div>
-          </li>
-        </ol>
-      </div>
-    </main>
-
-    <app-footer />
+              Go to event page
+            </nuxt-link>
+          </div>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import fetchContent from '~/lib/fetch-content';
-  import MainMenu from '~/components/main-menu/MainMenu';
-  import AppFooter from '~/components/app-footer/AppFooter';
 
   export default {
+    layout: 'globe',
     async middleware ({ store, redirect }) {
       const app = await import('~/static/data/app.json');
       store.commit('setDescription', app.default.description);
+      store.commit('setHighlightedEvent', app.default.highlightedEvent);
       store.commit('setNavigationLinks', app.default.navigationLinks);
     },
     async asyncData() {
@@ -56,6 +51,10 @@
             id
             startDate
             url
+            geolocation {
+              latitude
+              longitude
+            }
             image {
               url
             }
@@ -66,6 +65,10 @@
             id
             slug
             startDate
+            geolocation {
+              latitude
+              longitude
+            }
             image {
               url
             }
@@ -77,16 +80,48 @@
         ...await fetchContent(query),
       };
     },
-    components: { MainMenu, AppFooter },
+    computed: {
+      ...mapState(['highlightedEvent']),
+    },
+    data() {
+      return {
+        markers: [],
+      };
+    },
+    mounted () {
+      const allEvents = [...this.allInternalEvents, ...this.allExternalEvents];
+      this.markers = allEvents.map(event => {
+        return {
+          location: {
+            lat: event.geolocation.latitude,
+            lon: event.geolocation.longitude,
+          },
+          theme: {
+            slug: 'event',
+          },
+        };
+      });
+      this.$store.commit('replaceTheme', 'too-much');
+      this.$store.commit('replaceFeatures', this.markers);
+      this.$store.commit('enableInteraction');
+      this.$store.commit('enableGlobeAutoRotation');
+    },
   };
 </script>
 
 <style>
-  .event-preview {
-    margin: 2rem;
+  .layout-section--events {
+    position: relative;
+    margin-top: calc(var(--globe-spacing-default) + 50px);
   }
 
-  .event-preview__link {
-    font-weight: bold;
+  .layout-section--events:before {
+    content: '';
+    display: block;
+    position: absolute;
+    margin-top: -100px;
+    width: 100%;
+    height: 100px;
+    background: linear-gradient(0deg, #080808 0%, rgba(8, 8, 8, 0) 100%);
   }
 </style>

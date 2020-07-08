6a03a1267448b703c80297034766346fe81822f7
BasVoorhoede <66305989+BasVoorhoede@users.noreply.github.com>
2020-07-08 14:16:06 +0200

    Event chapter layout (#201)
    * Set up html and styling of narrative header for event page
    
    * Enable scrollTo behaviour when selecting a page
    
    * Add fallback for when no cover image is available
    
    * Set link in back button
    
    * Add content to chapter layout
    
    * Add spacing between multiple images
    
    * Get markdown from graphql query and make first paragraph bold using css
    
    Co-authored-by: Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>

--- components/page-body/PageBody.vue
+++ components/page-body/PageBody.vue
@@ -246,16 +246,6 @@ export default {
   display: inline-block;
 }
 
-.fixed-ratio {
-  padding: 0;
-  position: relative;
-  background-color: var(--blue-secondary);
-}
-
-.fixed-ratio > * {
-  position: absolute;
-}
-
 /*
 * style rules for a minimal print layout
 */

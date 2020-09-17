4b28f3697784a594b30656d607a9bdd9b90d2205
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2020-09-17 17:20:44 +0200

    Provide fallback for feature index page

--- pages/features/index.vue
+++ pages/features/index.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
-    <ul>
-      <li
-        v-for="feature in allFeatures"
-        :key="feature.slug"
-      >
-        <nuxt-link :to="`/features/${feature.slug}`">
-          {{ feature.title }}
-        </nuxt-link>
-      </li>
-    </ul>
-  </div>
+  <section class="layout-section">
+    <div class="layout-section__container layout-section__container--padded">
+      <h1 class="features__title">
+        Features
+      </h1>
+
+      <ul class="features__content">
+        <li
+          v-for="feature in allFeatures"
+          :key="feature.slug"
+          class="features__content-item"
+        >
+          <div class="features__icon">
+            <img
+              :src="feature.icon.url"
+              alt=""
+            >
+          </div>
+          <nuxt-link :to="`/features/${feature.slug}`">
+            {{ feature.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -24,6 +37,9 @@
             allFeatures {
               slug
               title
+              icon {
+                url
+              }
             }
           }
         `;
@@ -34,3 +50,37 @@
     },
   };
 </script>
+
+<style>
+  .features__title {
+    margin-bottom: 1rem;
+    font-size: 2rem;
+    font-weight: 900;
+    line-height: 1.2;
+    color: var(--blue-tertiary);
+  }
+
+  @media (--md-viewport) {
+    .features__title {
+      margin-bottom: 2rem;
+      font-size: 3.75rem;
+    }
+  }
+
+  .features__content {
+    list-style: none;
+  }
+
+  .features__content-item {
+    display: flex;
+    margin-bottom: 1rem;
+  }
+
+  .features__icon {
+    flex: 0 0 30px;
+  }
+
+  .features__icon img {
+    height: 20px;
+  }
+</style>
